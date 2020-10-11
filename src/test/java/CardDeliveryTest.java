96b318450aa1cf11904a2e8e698010a34c6e246e
camenzindmp <camenzindmp@gmail.com>
2020-10-11 21:54:39 +1000

    add successCase test

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+
+    @Test // тест проходит  успешно, но не написана логика на дату.
+    void successCase() {
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input[class=input__control]").setValue("Вл");
+        $(byText("Владивосток")).click();
+        $("[data-test-id=date] button[type=button]").click();
+        $("[class='popup__container'] [data-day='1604066400000']").click();
+        $("[data-test-id=name] [type=text]").setValue("Имя Фамилия");
+        $("[data-test-id=phone] [type=tel]").setValue("+79012345678");
+        $("[data-test-id=agreement]").click();
+        $("[type=button] [class='button__text']").click();
+        $("[data-test-id=notification]").waitUntil(Condition.visible, 10000);
+    }
+}
+
+
+
+
+
+
+
+
