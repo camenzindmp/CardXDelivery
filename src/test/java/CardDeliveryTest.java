dac7dabfdd0deb6aa70cf90fe0e7ff494985b080
Максим Пенкин <m.penkin@gmail.com>
2020-10-12 09:50:22 +1000

    tests modified (rework needed)

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -7,13 +7,13 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
 
-    @Test // тест проходит  успешно, но не написана логика на дату.
+  /*@Test
+        // реализация successCase с внедрением логики стороннего класса;
     void successCase() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input[class=input__control]").setValue("Вл");
         $(byText("Владивосток")).click();
-        $("[data-test-id=date] button[type=button]").click();
-        $("[class='popup__container'] [data-day='1604066400000']").click();
+        $("[data-test-id=date] [type=tel]").setValue(//тут ответ от dateCounter);
         $("[data-test-id=name] [type=text]").setValue("Имя Фамилия");
         $("[data-test-id=phone] [type=tel]").setValue("+79012345678");
         $("[data-test-id=agreement]").click();
@@ -21,9 +21,25 @@ public class CardDeliveryTest {
         $("[data-test-id=notification]").waitUntil(Condition.visible, 10000);
         // 1 вариант: написать логику календаря;
         // 2 вариант: листать страницу и тыкать на последнюю строчку всегда;
-    }
-}
+    }*/
 
+   /* @Test
+        // недоделанная реализация successCase без внедрения логики стороннего класса;
+    void successCase() {
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input[class=input__control]").setValue("Вл");
+        $(byText("Владивосток")).click();
+        $("[data-test-id=date] button[type=button]").click(); // клик на колендарь;
+        $("[class='popup__container'] [data-step='1']").click(); // переход на некс тпейдж календаря;
+        $("[class='popup__container'] [class='calendar__row']").shouldBe(Condition.visible); // найти строку в календаре (нужна доработка);
+        $("[class='calendar__day']").click(); // кликнуть на дату (нужна доработка);
+        $("[data-test-id=name] [type=text]").setValue("Имя Фамилия");
+        $("[data-test-id=phone] [type=tel]").setValue("+79012345678");
+        $("[data-test-id=agreement]").click();
+        $("[type=button] [class='button__text']").click();
+        $("[data-test-id=notification]").waitUntil(Condition.visible, 10000);
+    }*/
+}
 
 
 
